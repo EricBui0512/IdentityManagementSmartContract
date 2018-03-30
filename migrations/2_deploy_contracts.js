ee577f6a7bbb5f40fda6db569b275a3efbf1a2ce
Eric Bui <EricBui@ExSmart.co>
2018-03-30 13:13:19 +0800

    delete contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,9 @@
 var UserProfile = artifacts.require("UserProfile.sol");
 var NextIdNetwork = artifacts.require("NextIdNetwork.sol");
-var NextIdToken = artifacts.require("NextIdToken.sol");
 var FacetSmart = artifacts.require("FacetSmart.sol");
 
 module.exports = function(deployer) {  
   deployer.deploy(NextIdNetwork);
-  // deployer.deploy(NextIdToken);
   // deployer.deploy(UserProfile);
   // deployer.deploy(FacetSmart);
 };
